4097e0e16ad5163cf75460724d1c5557824fec2e
Jerome Dathueyt <j.dathueyt@weekera.fr>
2022-12-19 23:49:35 +0100

    ssr = false

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,24 @@
+<script context="module">
+	export const ssr = false;
+</script>
+
 <script>
 	import Modal from '../components/Modal.svelte';
 	import { stateStore } from '../stores/mainStore';
 	import Sidebar from '../components/Sidebar.svelte';
 	import { onMount } from 'svelte';
 	import { apiFetch } from '../api/fetcher';
-	import { sideBarData } from '../mock/datas'
+	import { sideBarData } from '../mock/datas';
 	import '../app.css';
 
 	// import { getStores, navigating, page, updated } from '$app/stores';
 
 	onMount(async () => {
-		const categories = await apiFetch('category', {METHOD: 'get'}).then(rep => rep?.data || sideBarData)
-		$stateStore.categories = categories
-	}); 
-
+		const categories = await apiFetch('category', { METHOD: 'get' }).then(
+			(rep) => rep?.data || sideBarData
+		);
+		$stateStore.categories = categories;
+	});
 </script>
 
 <div class="layout">
