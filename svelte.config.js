af6a2233c583e160a0d284d8f0f2cc9add66f828
Jerome Dathueyt <j.dathueyt@weekera.fr>
2022-12-20 00:06:40 +0100

    test update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html',
+			fallback: null,
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true,
 			out: 'build' 
 		})
 	}
